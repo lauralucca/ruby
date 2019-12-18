434861373a5c699936d7d686f614bad907a37d68
Laura Lourenco Lucca <llourenc@everis.com>
2019-12-18 14:08:24 -0300

    wip

---
+++ practical-OOD/gear.rb
@@ -0,0 +1,18 @@
+class Gear
+    private
+    attr_reader :chainring, :cog
+    
+    public
+    def initialize(chainring, cog)
+        @chainring = chainring
+        @cog       = cog
+    end
+end
+
+class Blinkered
+    def cog(gear)
+        gear.cog
+    end
+end
+
+puts Blinkered.new.cog(Gear.new(54,11))
